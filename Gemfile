27f12cd8acf83ba50d160ec9ea6d48b27b6198ba
Jason Woolard <beach180@Jason-Woolards-iMac.local>
2012-01-10 18:05:24 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+
+gem 'pg'
+
+gem 'jquery-rails'
+
+group :development do
+  gem 'rspec-rails', '2.8.0'
+end
+
+group :test do
+ gem 'rspec', '2.8.0'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '918b17f5b9834ac5c442997e12f40ff65d0187f2565c43aec05eb74ae2ed65a5532c4f64e7a5ff9b522d96ba4b6b67fa20a613117de623384d4ca7e2492fb3fb'
